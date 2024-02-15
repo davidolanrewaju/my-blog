dfc2cb16a0bb16fbe59655e7a5919887d6621ed8
David <davidolanrewaju538@gmail.com>
2024-02-15 16:35:36 +0100

    Fix google and fb button styling

--- login.php
+++ login.php
@@ -114,11 +114,11 @@ if (isset($_POST['submit'])) {
             </form>
             <div class="third-party-login">
                 <div class="google-container">
-                    <img class="google-logo" src="css/assets/google.svg" alt="google" />
+                    <img class="google-logo" src="css/assets/google.svg" alt="google"  width="40" height="40"/>
                     <p>SignUp using Google</p>
                 </div>
                 <div class="facebook-container">
-                    <img class="facebook-logo" src="css/assets/facebook.svg" alt="facebook" />
+                    <img class="facebook-logo" src="css/assets/facebook.svg" alt="facebook"  width="40" height="40"/>
                     <p>SignUp using Facebook</p>
                 </div>
             </div>
