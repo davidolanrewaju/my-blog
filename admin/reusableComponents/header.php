b642479bccf4b1f701dc098a20e663ee3ae13db9
David <davidolanrewaju538@gmail.com>
2024-02-21 12:10:33 +0100

    Refactor code

--- admin/reusableComponents/header.php
+++ admin/reusableComponents/header.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/add-posts.css">
+    <link rel="stylesheet" href="css/posts.css">
+    <link rel="stylesheet" href="css/published.css">
     <link rel="stylesheet" href="css/success.css">
     <title>TechInfo Admin</title>
 </head>
--- admin/reusableComponents/sidebar.php
+++ admin/reusableComponents/sidebar.php
@@ -1,3 +1,9 @@
+<?php 
+$type = isset($_GET['type']) ? $_GET['type'] : 'drafts';
+$post_id = isset($_GET['p_id']) ? $_GET['p_id'] : null;
+$types = array('view', 'edit', 'drafts', 'published');
+?>
+
 <div class="side-nav">
     <div class="logo">
         <h1>TechInfo</h1>
@@ -5,7 +11,15 @@
     </div>
     <div class="tabs">
         <a class="tab" href="./">Dashboard</a>
-        <a class="tab" href="posts.php">Posts</a>
+        <?php
+        // Loop through types and generate links dynamically
+        foreach ($types as $postType) {
+            $isActive = ($type == $postType) ? 'active' : ''; // Apply a different style for the active link
+
+            $link = ($postType == 'view' || $postType == 'edit') ? "posts.php?type=$postType&p_id=$post_id" : "posts.php?type=$postType";
+        }
+        echo "<a class='tab $isActive' href='$link'>Posts</a>";
+        ?>
         <a class="tab" href="add-posts.php">Add Posts</a>
         <a class="tab" href="comments.php">Comments</a>
         <a class="tab" href="categories.php">Categories</a>
