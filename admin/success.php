429c32655f535857e0707806963a0b4cde7cb4dc
David <davidolanrewaju538@gmail.com>
2024-02-23 14:26:50 +0100

    Refactor code

--- admin/success.php
+++ admin/success.php
@@ -1,11 +1,15 @@
-<?php include("./reusableComponents/session.php"); ?>
-<?php $status_message = isset($_GET['status']) ? urldecode($_GET['status']) : 'Operation completed successfully.';?>
-<?php include './reusableComponents/header.php'; ?>
+<?php 
+    include("./reusableComponents/session.php");
+    $status_message = isset($_GET['status']) ? urldecode($_GET['status']) : 'Operation completed successfully.';
+    include './reusableComponents/header.php';
+?>
 
 <div class="success-container">
     <div class="success-content">
         <img src="../admin/css/assets/success.png" alt="" width="100" height="100">
-        <p><?php echo $status_message; ?></p>
+        <p>
+            <?php echo $status_message; ?>
+        </p>
     </div>
 </div>
 
